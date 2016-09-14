5107f89a3357808551d8478f96ab3020d409e3da
Antonio Carlos Lembeck <tony.lembeck@gmail.com>
2016-09-14 17:30:01 -0300

    Correção das configurações do nome da Biblioteca de "droidAR" para "droidar" e correção da verção minima e de compilação do SDK.

--- app/src/main/java/br/com/android/sample/view/home/HomeActivity.java
+++ app/src/main/java/br/com/android/sample/view/home/HomeActivity.java
@@ -1,6 +1,11 @@
 package br.com.android.sample.view.home;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import org.androidannotations.annotations.AfterViews;
@@ -56,6 +61,10 @@ public class HomeActivity extends AppCompatActivity
     /**
      *
      */
+
+    private Location location;
+    private LocationManager locationManager;
+
     @UiThread
     @Click(R.id.buttonAR)
     public void onARClick()
@@ -64,10 +73,19 @@ public class HomeActivity extends AppCompatActivity
 
             public void addObjectsTo(GL1Renderer renderer, final World world, GLFactory objectFactory) {
 
+                if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED){
+
+                }else {
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
+
+
                 final Location l = new Location("");
 
-                l.setLatitude(-25.520911);
-                l.setLongitude(-54.581084);
+                l.setLatitude(location.getLatitude() -0.025911);
+                l.setLongitude(location.getLongitude());
                 Obj o = new GeoObj(l);
                 o.setComp(objectFactory.newArrow());
                 world.add(o);
