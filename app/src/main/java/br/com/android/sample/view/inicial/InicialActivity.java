3c4be890d3e60cbe1f311c7fae0023d195c7929f
Antonio Carlos Lembeck <tony.lembeck@gmail.com>
2016-09-15 01:40:03 -0300

    Criação da Tela Inicial

---
+++ app/src/main/java/br/com/android/sample/view/inicial/InicialActivity.java
@@ -0,0 +1,21 @@
+package br.com.android.sample.view.inicial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+
+import br.com.android.sample.R;
+import br.com.android.sample.view.authentication.AuthenticationActivity_;
+
+@EActivity(R.layout.activity_inicial)
+public class InicialActivity extends AppCompatActivity {
+
+    @UiThread
+    @Click(R.id.login)
+    public void onLoginClick(){
+        super.startActivity( new Intent(this, AuthenticationActivity_.class) );
+    }
+}
